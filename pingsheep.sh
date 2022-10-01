4ac9621993cc8092569dba1f0ce6f48ac2b7d0e2
BerserkPacho <contadogbl1806@gmail.com>
2022-10-01 20:49:47 -0300

    first prototype

--- pingsheep.sh
+++ pingsheep.sh
@@ -4,6 +4,17 @@ then
     tput setaf 6; echo "antar3s_pingsweep"
     echo "Please, insert an argument"
     tput setaf 1; echo "Use mode: $0 net"
+elif [ "$2" == "-r" ]
+then 	
+    for ip in {$3..$4}; 
+    i = 0;  
+    do 
+
+       tput setaf 2; echo ">"ping -c 1 $1.$ip | grep "64 bytes" | cut -d ":" ->
+       i = i + 1; 
+    done           
+
+	
 else
     for ip in {1..254};
     i = 0;  
